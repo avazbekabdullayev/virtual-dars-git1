35a8382601184536f48f6430a6ed648578e4d4c6
avazbekabdullayev <abdullayevavazbek834@gmail.com>
2024-11-15 23:57:01 +0500

    clear code

--- Program.cs
+++ Program.cs
@@ -1,7 +1,3 @@
 ﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+
 
